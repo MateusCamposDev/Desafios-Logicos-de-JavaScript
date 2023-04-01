7c7f6affaa40a63007ad10f5a4f814f2e5a4b17d
unknown <mateuscamposdev@gmail.com>
2023-04-01 00:23:47 -0300

    Desafio jS-Filter

--- js/Lista-De-Desafios-3.js
+++ js/Lista-De-Desafios-3.js
@@ -189,3 +189,66 @@ const approvedStudents = studentsResult.map((student) => {
 })
 
 console.log(approvedStudents)
+
+
+const listFilter = [
+    {name: "Mateus", vip: true},
+    {name: "Keite", vip: false},
+    {name: "Julia", vip: true},
+    {name: "João", vip: true},
+    {name: "Pedro", vip: false},
+    {name: "Brenda", vip: true},
+    {name: "Apolo", vip: false},
+]
+
+const justVips = list.filter ( client => {
+    return client.vip
+})
+
+console.log(justVips)
+
+const studentsFilter = [
+    {name: "Mateus", testGrade: 8},
+    {name: "Keite", testGrade: 10},
+    {name: "Julia", testGrade: 7},
+    {name: "João", testGrade: 4},
+    {name: "Pedro", testGrade: 2},
+    {name: "Brenda", testGrade: 5},
+    {name: "Apolo", testGrade: 6},
+]
+
+const newStudentList = studentsFilter.filter(student => {
+    return student .testGrade < 7
+})
+
+console.log(newStudentList)
+
+
+// Filtre e adicione no novo array com apenas os números pares e divisivel por 5.
+
+const listNumber = [20,3,234,12,17,541,6,87,275,1000];
+
+const newListNumber = listNumber.filter(numbers => {
+   if(numbers % 2 !== 0) return false
+   if(numbers % 5 !== 0) return false
+
+   return true
+})
+
+console.log(newListNumber)
+
+const companies = [
+    { name:'Samsung', marketValue: 50, CEO: 'Kim Hyun Suk', foundedOn: 1938},
+    { name: 'Microsoft', marketValue: 415, CEO: 'Satya Nadella', foundedOn: 1975},
+    { name: 'Intel', marketValue: 117, CEO: 'Brian Krzanich', foundedOn: 1968},
+    { name: 'facebook', marketValue: 383, CEO: 'Mark Zuckerberg', foundedOn: 2004},
+    { name: 'Spotify', marketValue: 30, CEO: 'Daniel EK', foundedOn: 2006},
+    { name: 'Apple', marketValue: 845, CEO: 'Tim Cook', foundedOn: 1976}
+]
+
+const newCompanies = companies.filter (company => {
+     if(company.foundedOn > 1975 && company.marketValue > 200) return true
+     else return false
+})
+
+console.log(newCompanies)
